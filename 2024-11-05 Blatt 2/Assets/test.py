7823c75838a6bfcc2c65cafffcf1471259c70a21
lenar <lenard.felix@outlook.com>
2024-11-11 12:52:47 +0100

    Aktualisiere Abgabe auf finalen Stand

--- 2024-11-05 Blatt 2/Assets/test.py
+++ 2024-11-05 Blatt 2/Assets/test.py
@@ -2,15 +2,7 @@ n = 2
 x1 = 0.2
 genauigkeit = 0.00001  # die fünf Nachkommastellen
 
-while True:
-    h = n / x1  # Berechnung von h
-    if h == x1:
-        print(f"x (näherungsweise Wurzel) gefunden: {x1}")
-        break
+while abs(n / x1 - x1) >= genauigkeit:
+    x1 = (x1 + n / x1) / 2
 
-    x_neu = (x1 + h) / 2  # neuer Näherungswert
-    if abs(x_neu - x1) < genauigkeit:
-        print(f"x (näherungsweise Wurzel) gefunden: {round(x_neu, 5)}")
-        break
-
-    x1 = x_neu  # Aktualisierung von x1 für die nächste Iteration
+print(f"x (näherungsweise Wurzel) gefunden: {round(x1, 5)}")
