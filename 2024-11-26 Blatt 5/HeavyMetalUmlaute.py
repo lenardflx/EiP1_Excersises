fb2d9069b8b4ede876781cf0a135661040ce6709
lenar <lenard.felix@outlook.com>
2024-11-28 14:29:57 +0100

    vereinfachung kurze syntax

--- 2024-11-26 Blatt 5/HeavyMetalUmlaute.py
+++ 2024-11-26 Blatt 5/HeavyMetalUmlaute.py
@@ -1,40 +1,35 @@
-UMLAUTS = {"a": "ä", "o": "ö", "u": "ü"}
+UMLAUTS = {"a": "ä", "A": "Ä", "o": "ö", "O": "Ö", "u": "ü", "U": "Ü"}
+
+def rep2indep(text):
+    return " ".join(map(lambda w,c=0: ''.join(UMLAUTS[l] if l in UMLAUTS and (c:=c+1)==2 else l for l in w),text.split()))
+
+def rep2type(text):
+    return " ".join(map(lambda w,c={"a":0,"o":0,"u":0}: ''.join(UMLAUTS[l] if l in UMLAUTS and (c.update({l.lower():
+        c[l.lower()] + 1}) or c[l.lower()] == 2) and not any(c.values()) >= 2 else l for l in w), text.split()))
 
 def replace_second_vowel_independently(text):
     def edit_word(word):
         c,res = 0, ""
         for l in word:
-            if l.lower() in UMLAUTS:
+            if l in UMLAUTS:
                 c += 1
                 if c == 2:
-                    l = UMLAUTS[l.lower()].upper() if l.isupper() else UMLAUTS[l.lower()]
+                    l = UMLAUTS[l]
             res += l
         return res
     return " ".join(map(edit_word, text.split()))
-
-def rep2indep(text):
-    return " ".join(map(lambda word: (lambda c=0: ''.join(
-            (UMLAUTS[l.lower()].upper() if l.isupper() else UMLAUTS[l.lower()]) if l.lower()
-            in UMLAUTS and (c := c + 1) == 2 else l for l in word))(),text.split()))
 
 def replace_second_vowel_by_type(text):
     def edit_word(word):
-        c, done, res = {"a": 0, "o": 0, "u": 0}, False, ""
+        c, res = {"a": 0, "o": 0, "u": 0}, ""
         for l in word:
-            if l.lower() in UMLAUTS and not done:
+            if l in UMLAUTS:
                 c[l.lower()] += 1
-                if c[l.lower()] == 2:
-                    l = UMLAUTS[l.lower()].upper() if l.isupper() else UMLAUTS[l.lower()]
-                    done = True
+                if c[l.lower()] == 2 and not any(c.values()) >= 2:
+                    l = UMLAUTS[l]
             res += l
         return res
     return " ".join(map(edit_word, text.split()))
-
-def rep2type(text):
-    return " ".join(map(lambda word: (lambda c={"a": 0, "o": 0, "u": 0}, done=False: ''.join(
-        (UMLAUTS[l.lower()].upper() if l.isupper() else UMLAUTS[l.lower()])
-        if l.lower() in UMLAUTS and not done and (c.update({l.lower(): c[l.lower()] + 1}) or c[l.lower()] == 2)
-        and (done := True) else l for l in word))(), text.split()))
 
 if __name__ == "__main__":
     for x in ["Motorhead", "Heavy Metal Umlaute", "aaauuu oooo iii kukukuku", "Python super Programmiersprache"]:
