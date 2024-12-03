a5a242c0ae6eefa87db465117fbb5d2a4bf237c4
lenar <lenard.felix@outlook.com>
2024-12-03 16:31:34 +0100

    Formattierung und vereinfachung

--- 2024-12-03 Blatt 6/Midterm.py
+++ 2024-12-03 Blatt 6/Midterm.py
@@ -2,33 +2,17 @@
 print("Aufgabe 1:")
 ROMAN_NUMBERS = {1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10: "X"}
 get_roman_number = lambda n: ROMAN_NUMBERS.get(n, None)
-
-def sum_roman(x: int, y: int) -> str:
-    if 0 < x < 11 and 0 < y < 11 and (x + y) <= 10:
-        return get_roman_number(x + y)
-    return "Fehler"
-
-print(f"sum_roman(2, 4) = {sum_roman(2, 4)}")
-print(f"sum_roman(12, 3) = {sum_roman(12, 3)}")
+sum_roman = lambda x, y: get_roman_number(x + y) if 0 < x < 11 and 0 < y < 11 and (x + y) <= 10 else "Fehler"
+print(f"sum_roman(2, 4) = {get_roman_number(2)} + {get_roman_number(4)} = {sum_roman(2, 4)}")
+print(f"sum_roman(12, 3) = {get_roman_number(12)} + {get_roman_number(3)} = {sum_roman(12, 3)}")
 
 # 2)
 print("\nAufgabe 2:")
-def trib(n: int) -> int:
-    if n in [1, 2]:
-        return 1
-    if n == 3:
-        return 2
-    return trib(n - 1) + trib(n - 2) + trib(n - 3)
-
+trib = lambda n: 1 if n in [1,2] else 2 if n == 3 else trib(n-1) + trib(n-2) + trib(n-3)
 print(f"trib(2) = {trib(2)}")
 print(f"trib(12) = {trib(12)}")
 
 # 3)
 print("\nAufgabe 3:")
-def search_max(some_list: list) -> int:
-    if len(some_list) == 1:
-        return some_list[0]
-    other_max = search_max(some_list[1:])
-    return some_list[0] if some_list[0] > other_max else other_max
-
+search_max = lambda lst: lst[0] if len(lst) == 1 or lst[0] > (m := search_max(lst[1:])) else m
 print(f"search_max([91,812,3,34,6345,23,5,71]) = {search_max([91,812,3,34,6345,23,5,71])}")
--- 2024-12-03 Blatt 6/SiebEratosthenes.py
+++ 2024-12-03 Blatt 6/SiebEratosthenes.py
@@ -1,35 +1,37 @@
 def iter_get_primes(n: int) -> list:
-    primes = []
-    state = [True] * n
-    for i in range(2, n):
+    primes, state = [], [True]*n
+    for i in range(2,n):
         if state[i]:
             primes.append(i)
             for j in range(i**2, n, i):
                 state[j] = False
     return primes
 
-def semi_rec_get_primes(primes: list, current: int = 0) -> list:
-    if current >= len(primes) or primes[current] ** 2 > primes[-1]:
-        return primes
-    primes = [x for x in primes if x == primes[current] or x % primes[current] != 0]
-    return semi_rec_get_primes(primes, current + 1)
+def semi_rec_get_primes(n: int) -> list:
+    def rec(primes, current=0):
+        if current >= len(primes) or primes[current] ** 2 > primes[-1]:
+            return primes
+        primes = [x for x in primes if x == primes[current] or x % primes[current] != 0]
+        return rec(primes, current + 1)
+    return rec(list(range(2, n+1)))
 
-def rec_get_primes(primes: list, current: int = 0) -> list:
-    if current >= len(primes) or primes[current] ** 2 > primes[-1]:
-        return primes
-    def rm_multiples(curr_primes: list, nbr: int) -> list:
-        if not curr_primes:
-            return []
-        nxt = rm_multiples(curr_primes[1:], nbr)
-        return [curr_primes[0]] + nxt if curr_primes[0] == nbr or curr_primes[0] % nbr != 0 else nxt
-    return rec_get_primes(rm_multiples(primes, primes[current]), current + 1)
+def rec_get_primes(n: int) -> list:
+    def rec(primes, current=0):
+        if current >= len(primes) or primes[current] ** 2 > primes[-1]:
+            return primes
+        def rm_multiples(curr_primes: list, nbr: int) -> list:
+            if not curr_primes:
+                return []
+            nxt = rm_multiples(curr_primes[1:], nbr)
+            return [curr_primes[0]] + nxt if curr_primes[0] == nbr or curr_primes[0] % nbr != 0 else nxt
+        return rec(rm_multiples(primes, primes[current]), current + 1)
+    return rec(list(range(2, n+1)))
 
 # Nach dem Prinzip: Ist n keine Primzahl, so hat n einen Teiler a mit a <= sqrt(n) (credits an Prof. de Jong)
 def simple_get_primes(n: int) -> list:
     return [i for i in range(2, n+1) if all(i % j != 0 for j in range(2, int(i ** 0.5) + 1))]
 
 n = 100
-prime_res = [iter_get_primes(n), semi_rec_get_primes(list(range(2, n + 1))),
-             rec_get_primes(list(range(2, n + 1))), simple_get_primes(n)]
-print("\n".join(f"Primzahlen {i}) bis {n}: {p}" for i, p in enumerate(prime_res, 1)))
-print(f"Alle Identisch?: {all(p == prime_res[0] for p in prime_res)}")
+res = [f(n) for f in [iter_get_primes, semi_rec_get_primes, rec_get_primes, simple_get_primes]]
+print("\n".join(f"Primzahlen {i}) bis {n}: {p}" for i, p in enumerate(res, 1)))
+print(f"Alle Identisch?: {all(p == res[0] for p in res)}")
