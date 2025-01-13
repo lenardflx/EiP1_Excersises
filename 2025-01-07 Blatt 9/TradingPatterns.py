2eb918ca8e88f7ceb1845e379b01de6b11742020
Lönard <lenard.felix@outlook.com>
2025-01-13 17:17:35 +0100

    Blatt 9 final

--- 2025-01-07 Blatt 9/TradingPatterns.py
+++ 2025-01-07 Blatt 9/TradingPatterns.py
@@ -7,31 +7,24 @@ prices = [d[5] for d in data]
 
 def is_double_top(arr_part):
     tolerance = 0.01
-    d1, d2, d3, d4 = arr_part
-    return (d2 > d1 and
-            d3 < d2 and
-            abs(d4-d2) <= tolerance)
+    return (arr_part[0] < arr_part[1] and
+            arr_part[2] < arr_part[1] and
+            abs(arr_part[3]-arr_part[1]) < tolerance)
 
 def check_prediction(arr_part):
-    return arr_part[4] < arr_part[3]
+    return arr_part[3] > arr_part[4]
 
-def analyze_double_top_patterns(dates, prices):
-    predictions = []
-    for i in range(len(prices)-4):
-        if is_double_top(prices[i:i+4]):
-            print(f"Double top pattern found: {dates[i].strftime('%d.%m.%Y')}")
-            prediction = check_prediction(prices[i:i+5])
-            predictions.append(prediction)
-    if predictions:
-        print(f"\nAccuracy: {np.mean(predictions):.2%} ({sum(predictions)}/{len(predictions)})")
-    else:
-        print("No double top patterns found.")
-
-analyze_double_top_patterns(dates, prices)
+predictions = []
+for i in range(len(prices)-4):
+    if is_double_top(prices[i:i+4]):
+        print(f"Double top pattern found: {dates[i].strftime('%d.%m.%Y')}")
+        predictions.append(check_prediction(prices[i:i+5]))
+print("\nAccuracy:", f"{np.mean(predictions):.2%} ({sum(predictions)}/{len(predictions)})"
+      if predictions else "No double top patterns.")
 
 # Aufgabe 5
 # Das Ergebnis ist vom Toleranzwert abhängig, der je nach Markt angepasst werden müsste.
 # Schon kleine Änderungen des Wertes führen einer komplett anderen Anzahl an gefundenen Mustern.
 # Außerdem werden wichtige Faktoren wie Markttrends von der reinen Berechnung nicht berücksichtigt.
 # Die Trefferquote ist in unserem Fall sehr schlecht, wodurch den Ergebnissen nicht zu vertrauen ist.
-# Jedoch kann ein solcher Algorithmus zur Unterstützung von eigener Analyse verwendet werden.
+# Jedoch kann so ein Algorithmus zur Hilfe verwendet werden.
