3f5bbc54be0ba2c2838150944fe2709b078d1f0a
Lönard <lenard.felix@outlook.com>
2025-01-24 14:45:07 +0100

    Blatt 11 fertig 2

--- 2025-01-21 Blatt 11/Backtracking.py
+++ 2025-01-21 Blatt 11/Backtracking.py
@@ -3,6 +3,7 @@ from jguvc_eip import basic_io as bio, image_objects as iobj
 
 FILL_COL = {" ": (255, 255, 255), "E": (255, 0, 0), "A": (0, 0, 255),
     "B": (80, 200, 250), "C": (220, 100, 100), "P": (100, 200, 140), ".": (0, 0, 0)}
+PX_SIZE = 10
 
 def read_map(filename):
     return [list(line.strip()) for line in open(filename)]
@@ -10,11 +11,12 @@ def read_map(filename):
 def get_start(field):
     return next((x, y) for y, row in enumerate(field) for x, cell in enumerate(row) if cell == "E")
 
-def print_map(field, size=20, curr_pos=None):
+def print_map(field, size=PX_SIZE, curr_pos=None):
     stack = [iobj.HorizontalStack([iobj.Rectangle(size, size, FILL_COL[c]) for c in row]) for row in field]
     model = iobj.VerticalStack(stack)
     if curr_pos:
-        circ = iobj.Translate(iobj.Circle(10, fill_color=FILL_COL["P"]), curr_pos[0]*size+5, curr_pos[1]*size+5)
+        off_x, off_y = curr_pos[0]*size+(size//4), curr_pos[1]*size+(size//4)
+        circ = iobj.Translate(iobj.Circle(size//2, fill_color=FILL_COL["P"]), off_x, off_y)
         model = iobj.Overlay([model, circ])
     bio.draw_object(model, 50, 50)
 
@@ -36,7 +38,7 @@ def find_way(field, path):
 
 if __name__ == "__main__":
     bio.start()
-    maze = read_map("maze_big.txt")  # read_map("maze_small.txt")
+    maze = read_map("giant_test_maze.txt")
     route = find_way(maze, [get_start(maze)])
 
     bio.print_message("Found route: " + " → ".join(map(str, route)))
